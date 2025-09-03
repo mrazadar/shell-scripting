2d8bcbb5c28069791fca951492913b261072734c
Raza Dar <mrazadar@gmail.com>
2025-09-04 02:54:41 +0500

    Initial commit

---
+++ first-script.sh
@@ -0,0 +1,15 @@
+# My first shell script program...
+
+echo This is my first shell script. 
+
+echo System defined variable  
+echo $USER
+
+# creating and accessing User defined variable
+
+echo User defined variable 
+guess_name="Devspeak.co"
+
+echo $variable_name
+
+echo End of my first shell script. 
