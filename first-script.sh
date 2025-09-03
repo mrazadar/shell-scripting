e789342dd874feba07c4716f3246fe53d6d7f0e2
Raza Dar <mrazadar@gmail.com>
2025-09-04 03:07:51 +0500

    updated script

--- first-script.sh
+++ first-script.sh
@@ -1,3 +1,12 @@
+# You can specify which shell the script will use, even if the script is executed from another shell terminal.
+# To do this, add "#!" on top of the script file, followed by the absolute path of the shell of choice. 
+# To specify bash as an interpreter, Add the following line on top of the shell script.
+
+#!/bin/bash
+
+# This line is called the shebang line.
+
+
 # My first shell script program...
 
 echo This is my first shell script. 
@@ -8,8 +17,8 @@ echo $USER
 # creating and accessing User defined variable
 
 echo User defined variable 
-guess_name="Devspeak.co"
+org_name="Devspeak.co"
 
-echo $variable_name
+echo $org_name
 
 echo End of my first shell script. 
