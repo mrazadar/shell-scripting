a7c1b125f6459043446ea93e96e2dab56529b744
Raza Dar <mrazadar@gmail.com>
2025-09-04 21:41:37 +0500

    Testing with switch statement

--- condtional-script.sh
+++ condtional-script.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
 
 x=10
-y=11
+y=10
+z=10
+CARS="BMW"
+
 if [ $x -ne $y ]
-then 
-echo "Not Equal"
+then	
+	echo "Not Equal"
+else
+	if [ $x -eq $z ] 
+	then 
+		echo "Equal to $z"
+	else
+		echo "Not equal to $z"
+	fi
 fi
+
+# SWITCH STATEMENTS 
+
+case $CARS in 
+	#case 1
+	"HONDA") echo "Headquarters - Japan, Tokyo" ;;
+
+	#case 2
+	"AUDI") echo "Headquarters - Germany, Munich" ;;
+
+	#case 3 
+	"BMW") echo "Headquarters - Netherland, Amsterdam" ;;
+esac
+
+
