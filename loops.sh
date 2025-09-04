41755b3593721aa020eae7ec85d8c73b5f89ae61
Raza Dar <mrazadar@gmail.com>
2025-09-04 23:34:58 +0500

    more with loops using until

--- loops.sh
+++ loops.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 x=2
+y=1
+
+# create a space separated list
+CARS="Hond Bugati BMW Mercedez"
+
+# while loop
 while [ $x -lt 6 ]
 do 
 	echo $x 
 	x=$((x + 1))  # Correct Arithmetic assignment
+done
+
+# for loop
+# can be 'break' or 'continue' the for loop
+for var in $CARS
+do 
+	echo "I have a $var car"
+done
+
+# until loop
+# loop until the condition becomes truthy
+until [ $y -gt 10 ]
+do
+	#print the value of y
+	echo "The value of Y is: $y"
+
+	y=$((y+1)) # arithemtic expression 
 done 
+
+
