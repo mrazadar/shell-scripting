1f666474619452dd70d495904e0ced9964a15a84
Raza Dar <mrazadar@gmail.com>
2025-09-05 00:15:44 +0500

    more on reading arguments'

--- args.sh
+++ args.sh
@@ -4,3 +4,13 @@ echo "no of arguments is $#"
 echo "name of the script is $0"
 echo "first argument is $1"
 echo "second argument is $2"
+
+b=$1
+c=$2
+d=`pwd`
+e=$(pwd)
+
+echo "Var b is: $b" 
+echo "Var c is: $c" 
+echo "Var d is: $d" 
+echo "Var e is: $e" 
